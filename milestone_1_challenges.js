9c3f5d226c147169d876d7b92dc04e1302ba2b1c
ginakena <makenareginam@gmail.com>
2025-05-20 15:37:43 +0300

    Challenge 2. converting minutes to seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -2,5 +2,13 @@
 function addition (num1, num2) {
     return num1+num2; 
 }
-console.log(addition(3, 5));
-console.log(addition(-6, 9));
+
+//console.log(addition(3, 5)); //8
+//console.log(addition(-6, 9)); //3
+
+//converting minutes to seconds
+function convert (minutes) {
+    return minutes*60;
+}
+console.log(convert(5));
+console.log(convert(2));
