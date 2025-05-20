afbf6d3fc7723db8d89657a2675174b7c41b1f55
ginakena <makenareginam@gmail.com>
2025-05-20 15:46:26 +0300

    Challenge 3. finding perimeter

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,4 +1,4 @@
-//Sum of two numbers
+//Challenge 1:Sum of two numbers
 function addition (num1, num2) {
     return num1+num2; 
 }
@@ -6,9 +6,16 @@ function addition (num1, num2) {
 //console.log(addition(3, 5)); //8
 //console.log(addition(-6, 9)); //3
 
-//converting minutes to seconds
+//Challenge 2:converting minutes to seconds
 function convert (minutes) {
     return minutes*60;
 }
-console.log(convert(5));
-console.log(convert(2));
+//console.log(convert(5)); //300
+//console.log(convert(2)); //120
+
+//Challenge 3:Perimeter of a rectangle
+function findPerimeter (length, width) {
+    return 2*(length+width);
+}
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
