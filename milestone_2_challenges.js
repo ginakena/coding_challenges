10f5dd8e1f08a1226819bee5394ac74ba1e0dd51
ginakena <makenareginam@gmail.com>
2025-05-22 09:46:26 +0300

    challenge 2: Find Maximum value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -8,5 +8,17 @@ function sumOfPositives(arr) {
     }
     return sum;
 }
-console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+//console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+//Challenge 2: Write a function called findMax that takes an array of numbers and returns the maximum number in the array. Don’t use built-in functions like Math.max().
+function findMax(arr) {
+    let max = arr[0];
+    for (let i=1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([3, 7, 2, 9, 5])); 
         
